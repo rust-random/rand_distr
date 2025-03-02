0048240152c74aa2c2e723135b3dabc350baf838
Benjamin Lieser <benjamin.lieser@stud.uni-goettingen.de>
2025-03-02 10:22:17 +0100

    documentation for MultiDistr

--- src/multi/mod.rs
+++ src/multi/mod.rs
@@ -3,6 +3,8 @@
 //! We provide a trait `MultiDistribution` which allows to sample from a multi-dimensional distribution without extra allocations.
 //! All multi-dimensional distributions should implement this trait addidionally to the `Distribution` trait returning a `Vec` of samples.
 
+/// This trait allows to sample from a multi-dimensional distribution without extra allocations.
+/// Typically distributions will implement `MultiDistribution<[F]>` where `F` is the type of the samples.
 pub trait MultiDistribution<S> {
     fn sample<R : Rng + ?Sized, S : ?Sized>(&self, rng: &mut R, output: &mut S);
 }
