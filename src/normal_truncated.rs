f0b29b35872c4f2efb5b45b2491ce8157377e482
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2025-10-27 15:10:49 +0100

    one sided case

--- src/normal_truncated.rs
+++ src/normal_truncated.rs
@@ -48,3 +48,39 @@ impl Distribution<f64> for NormalTruncatedRejection {
         sample
     }
 }
+
+struct NormalTruncatedOneSided {
+    alpha_star: f64,
+    lower_bound: f64,
+    exp_distribution: crate::Exp<f64>,
+    mu: f64,
+    sigma: f64,
+}
+
+impl NormalTruncatedOneSided {
+    fn new(mu: f64, sigma: f64, lower: f64) -> Self {
+        let alpha = (lower - mu) / sigma;
+        let alpha_star = (alpha + (alpha.powi(2) + 4.0).sqrt()) / 2.0;
+        let lambda = alpha_star;
+        NormalTruncatedOneSided {
+            alpha_star,
+            lower_bound: lower - mu,
+            exp_distribution: crate::Exp::new(lambda).unwrap(),
+            mu,
+            sigma,
+        }
+    }
+}
+
+impl Distribution<f64> for NormalTruncatedOneSided {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> f64 {
+        loop {
+            let z = self.exp_distribution.sample(rng) + self.lower_bound;
+            let u: f64 = rng.random();
+            let rho = (-0.5 * (z - self.alpha_star).powi(2)).exp();
+            if u <= rho {
+                return self.mu + self.sigma * z;
+            }
+        }
+    }
+}
